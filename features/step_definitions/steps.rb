eb7f90865963429f64d107c40692e5a506f6c379
Hao Li <hao.li@vodafone.com.au>
2016-03-22 14:10:29 +0800

    Implement a step

---
+++ features/step_definitions/steps.rb
@@ -0,0 +1,15 @@
+Given(/^I'm signed up as "([^"]*)"$/) do |name|
+  first_name, last_name = name.split ' '
+  visit new_user_path
+  fill_in :user_first_name, with: first_name
+  fill_in :user_last_name, with: last_name
+  click_button 'Create User'
+end
+
+When(/^I create a project without a name$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^the project should be called 'Hao Li's project'$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
