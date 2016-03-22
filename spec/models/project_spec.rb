ef882f03c4841925a569aa69051ce0a33eb84e8b
Hao Li <hao.li@vodafone.com.au>
2016-03-22 15:31:13 +0800

    refactor the unit test

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -3,9 +3,10 @@ require 'spec_helper'
 
 describe Project do
   it 'should be named after the user if no is set' do
-    user = User.new first_name: 'Hao', last_name: 'Li'
+    first_name, last_name = 2.times.map { random_name }
+    user = User.new first_name: first_name, last_name: last_name
     project = Project.new
     project.user = user
-    expect(project.name).to eq("Hao Li's project")
+    expect(project.name).to eq("#{first_name} #{last_name}'s project")
   end
 end
